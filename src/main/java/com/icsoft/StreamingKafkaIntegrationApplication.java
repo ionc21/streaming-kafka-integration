a41973cc9837db26309ff85f5c44bd601878db20
Ion Ciobanu <I00577@uniper.energy>
2022-09-01 15:02:39 +0200

    Using CommandLineRunner|ApplicationRunner to execute the job

--- src/main/java/com/icsoft/StreamingKafkaIntegrationApplication.java
+++ src/main/java/com/icsoft/StreamingKafkaIntegrationApplication.java
@@ -1,16 +1,36 @@
 package com.icsoft;
 
 import com.icsoft.service.ProduceConsumeService;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StreamingKafkaIntegrationApplication {
 
-    public static void main(String[] args) throws Exception {
+   /* public static void main(String[] args) throws Exception {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(StreamingKafkaIntegrationApplication.class);
         ProduceConsumeService produceConsumeService = applicationContext.getBean(ProduceConsumeService.class);
         produceConsumeService.executeJob();
+    }*/
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingKafkaIntegrationApplication.class, args);
+    }
+
+/*
+    @Bean
+    CommandLineRunner commandLineRunner(ProduceConsumeService service) {
+        return args -> service.executeJob();
+    }
+*/
+
+    @Bean
+    ApplicationRunner applicationRunner(ProduceConsumeService service) {
+        return args -> service.executeJob();
     }
 }
